4927bdf721c82aa5760a84ad66b39b8e43875d90
Dharyaj11 <dharyajasuja2003@gmail.com>
2023-11-01 21:38:59 +0530

    updated createshop and completed backend api

--- client/Routes/CreateShop.js
+++ client/Routes/CreateShop.js
@@ -24,4 +24,44 @@ router.post("/newshops",async(req,res)=>{
   }
   })
 
+  router.put("/updateshops/:id",async(req,res)=>{
+
+    try {
+      // console.log(global.food_items);
+      console.log(req.body);
+      const result=await db.query("UPDATE shop SET name = $1, location = $2, price_range = $3 where id = $4 returning *",[req.body.name,req.body.location,req.body.price_range,req.params.id]);
+      console.log(result.rows[0]);
+      res.status(200).json({
+        status:"success",
+        result: result.rows.length,
+        data: {
+          shops:result.rows[0],
+        }
+      });
+  } catch (error) {
+      console.log(error.message);
+      res.send("Server Error")
+      
+  }
+  })
+  router.delete("/deleteshops/:id",async(req,res)=>{
+
+    try {
+      // console.log(global.food_items);
+      console.log(req.body);
+      const result=await db.query("DELETE FROM shop where id = $1", [req.params.id,]);
+      console.log(result.rows[0]);
+      res.status(200).json({
+        status:"success",
+        result: result.rows.length,
+        data: {
+          shops:result.rows[0],
+        }
+      });
+  } catch (error) {
+      console.log(error.message);
+      res.send("Server Error")
+      
+  }
+  })
   module.exports = router;
